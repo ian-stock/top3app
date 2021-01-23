4979b10185fae19438a84ef04c85594d4cfb1f61
Ian Stock <i_stock@hotmail.com>
2021-01-23 22:07:58 +0000

    multi-room complete

--- server.js
+++ server.js
@@ -10,31 +10,51 @@ http.listen(port, function(){
     console.log('listening on *:' + port);
 });
 
-//functions - newgame, joingame, startgame, submitanswer, revealanswer
+//functions - newgame, joingame, startgame, submitanswer, showanswers, revealuser
 
 io.on('connection', function(socket){
 
+  console.log("user connected: socketid: " + socket.id);
+
   socket.onAny((event, data) => {
     switch (event) {
-      case 'joingame': 
-        //socket.join(data);
-        console.log(socket.rooms)
-        console.log('emit: ' + event + " | " + data);
+      case 'newgame': 
+        socket.join(data);
+        console.log(socket.rooms);
+        console.log('emit: newgame | ' + data);
         break;
       case 'joingame': 
-        //socket.join(data);
-        console.log(socket.rooms)
-        console.log('emit: ' + event + " | " + data);
+        socket.join(data);
+        console.log(socket.rooms);
+        console.log('emit: joingame | ' + data);
+        break;
+      case 'submitanswer': 
+        console.log(socket.rooms);
+        console.log("emit: submitanswer, room: " + data[0] + " | " + socket.id);
+        io.to(data[0]).emit('submitanswer', data);
+        //io.emit('submitanswer', data);
         break;
       case 'showanswers': 
-        //io.to(data[0]).emit(event, data);
-        io.emit(event, data);
-        console.log('emit: ' + event + " | " + data.toString());
+        io.to(data[0]).emit('showanswers', data);
+        console.log("emit: showanswers, room: " + data[0] + " | " + socket.id);
+        break;
+      case 'revealuser': 
+        io.to(data[0]).emit('revealuser', data);
+        console.log("emit: revealuser, room: " + data[0] + " | " + socket.id);
+        break;
+      case 'leavegame': 
+        socket.leave(data);
+        console.log(socket.rooms);
+        console.log('emit: leavegame | ' + data);
+        break;
+      case 'disconnect': 
+        console.log("user disconnected: socketid: " + socket.id);
+        break;
       default: 
-        //io.to(data[0]).emit(event, data);
-        io.emit(event, data);
+        io.to(data[0]).emit(event, data);
         console.log('emit: ' + event + " | " + data);
     }
   });
+
 });
 
